ec1e8d9e6893038c991a66ab7d788497cb5e2c9f
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-08 08:44:51 -0500

    Init React complete guide

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+  state = {
+    persons: [
+      { id: '111', name: 'David', age: 25 },
+      { id: '222', name: 'Josh', age: 30 },
+      { id: '333', name: 'Stephany', age: 28 },
+    ],
+    showPersons: false
+  };
+
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(person => {      
+      return person.id === id;
+    })
+
+    const person = {...this.state.persons[personIndex]};
+    // OR const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // Always update your state in an immutable fashion
+    // const persons = this.state.persons.slice();
+    //  OR
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+
+    this.setState({ persons });
+  }
+
+  togglePersonsHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons })
+  }
+
+  render() {
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App.</h1>
+        <p>It's really working!</p>
+        <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
